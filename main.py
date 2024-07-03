b76db8a4e61ae90649a2b4c3680169a09bdb9581
alex <whichevercub05@gmail.com>
2024-07-03 23:24:18 +0100

    debugging

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ def provideSolutions(polygon):
 
 def works(solution):
     # checks if any lines in solution intersect
-    print("testing: ", solution)
+    # print("testing: ", solution)
     length = len(solution)
     for i in range(len(solution)):
-        line1 = [solution[i], solution[(i+1)%length]]
-        for j in range(i, len(solution)):
+        line1 = [solution[i], solution[(i+1)]]
+        for j in range(i+1, len(solution)):
             line2 = [solution[j], solution[(j+1)%length]]
             if intersects(line1, line2):
                 return False
@@ -38,6 +38,8 @@ def works(solution):
 # https://stackoverflow.com/questions/3838329/how-can-i-check-if-two-segments-intersect
 def intersects(line1, line2):
     # line = start and end points [[x1, y1], [x2, y2]]
+    print("line1: ", line1, " line2: ", line2)
+    
     ls1 = LineString(line1)
     ls2 = LineString(line2)
     if ls1.intersects(ls2):
